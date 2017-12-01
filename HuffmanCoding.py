f40056cb79e53ee841a970a6abb8d24511226abc
Arman Ashrafian <arman-ashrafian@users.noreply.github.com>
2017-11-30 17:28:32 -0800

    formatting

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -1,15 +1,15 @@
-	# Arman Ashrafian
-	# 11-30-2017
+# Arman Ashrafian
+# 11-30-2017
 
-	# This file implements everything needed for Lossless Huffman Coding.
-	# HuffmanCoding takes the full path of file to be compressed and builds
-	# the neccessary codes for encoding.
+# This file implements everything needed for Lossless Huffman Coding.
+# HuffmanCoding takes the full path of file to be compressed and builds
+# the neccessary codes for encoding.
 
-	# compress()        - Returns the path of the compressed file (.bin format)
-	#
-	# decompress(path)  - Requires path of file to be decompressed. Must be called
-	#                     from same object that called compressed because the code
-	#                     mapping is a data member of the object.  
+# compress()        - Returns the path of the compressed file (.bin format)
+#
+# decompress(path)  - Requires path of file to be decompressed. Must be called
+#                     from same object that called compressed because the code
+#                     mapping is a data member of the object.  
 
 import os
 import heapq
